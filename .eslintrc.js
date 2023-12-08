620fe1a6a2e1faad5041f2307f9acbd153ce1eed
evaldas.laureckas@gmail.com <evaldas.laureckas@gmail.com>
2023-12-08 06:53:00 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended'
   ],
   rules: {}
